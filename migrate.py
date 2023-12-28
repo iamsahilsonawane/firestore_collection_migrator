48f4ec1579e152f5196c283cb2e26c92590023ca
Sahil Sonawane <iamsahilsonawane@gmail.com>
2023-12-28 14:41:03 +0530

    Fixes

--- migrate.py
+++ migrate.py
@@ -6,13 +6,13 @@ from_cred = credentials.Certificate("service_accounts/from.json")
 to_cred = credentials.Certificate("service_accounts/to.json")
 
 default_app = initialize_app(from_cred, name="from-app")
-other_app = initialize_app(to_cret, name="to-app")
+other_app = initialize_app(to_cred, name="to-app")
 
 from_db = firestore.client(default_app)
 to_db = firestore.client(other_app)
 
-collection_ref = from_db.collection(sys.argv[0])
-destination_collection_ref = to_db.collection(sys.argv[0])
+collection_ref = from_db.collection(sys.argv[1])
+destination_collection_ref = to_db.collection(sys.argv[1])
 
 # transfer documents
 for doc in collection_ref.stream():
